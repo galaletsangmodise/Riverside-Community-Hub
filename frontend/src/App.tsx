53cbf11e1f40adb892e1e1ce33638e57daf6814f
galaletsangmodise <galaletsangmodise71@gmail.com>
2026-09-13 19:26:57 -0700

    fix: correct import path for Navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/NavBar';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { Booking } from './pages/Booking';
